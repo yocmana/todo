e09c0755148b763d9f2edc0301df4fd5c9366b19
yocmana <yocmana32@gmail.com>
2024-02-12 00:43:45 +0900

    1st コミット

---
+++ backend/src/test/java/com/jaunebrillant/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaunebrillant.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
